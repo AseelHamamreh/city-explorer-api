f783aa99b7555cde147ead007e9c384028dc8f67
Aseel Hamamreh <aseel.hamamreh@hotmail.com>
2021-05-06 03:17:13 +0300

    lab08

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const weather = require ('./data/weather.json');
+const superagent = require('superagent');
+
 
 const cors = require ('cors');
 app.use(cors());
@@ -10,15 +12,22 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.get('/weather', function (req, res) {
-  const dataArray = weather.data.map(data => new WeatherData(data));
-  res.send(dataArray)
-})
+app.get('/weather', (req, res) => {
+      const weatherBitUrl = `https://api.weatherbit.io/v2.0/forecast/daily?key=0bda87c6bdef4494a1e2e0301953a301&lat=${req.query.lat}&lon=${req.query.lon}`;
+      superagent.get(weatherBitUrl).then(weatherBitData => {
+          const arrOfData = weatherBitData.body.data.map(data => new WeatherData(data));
+          res.send(arrOfData);
+      });
+});
 
 class WeatherData{
   constructor(data){
     this.date=data.valid_date;
     this.description=data.weather.description;
+    this.highT=data.high_temp;
+    this.lowT=data.low_temp;
   }
 }
-app.listen(3007)
+
+
+app.listen(6070)
